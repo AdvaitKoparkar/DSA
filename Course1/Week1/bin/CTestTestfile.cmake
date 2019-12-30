20eed52cd380033c47b4db6b3b0fa443e3f35708
advait <advaitkoparkar@gmail.com>
2019-12-30 10:22:38 -0500

    merge sort implemented

---
+++ Course1/Week1/bin/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/Advait/Handouts_and_assignments/Fall_2019_coursework/DSA/Course1/Week1
+# Build directory: /mnt/d/Advait/Handouts_and_assignments/Fall_2019_coursework/DSA/Course1/Week1/bin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_merge_sort "/mnt/d/Advait/Handouts_and_assignments/Fall_2019_coursework/DSA/Course1/Week1/bin/test/test_merge_sort")
+subdirs("test")
---
+++ Course1/Week1/bin/test/CMakeFiles/test_merge_sort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_merge_sort.dir/test_merge_sort.cpp.o"
+  "test_merge_sort.pdb"
+  "test_merge_sort"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_merge_sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
